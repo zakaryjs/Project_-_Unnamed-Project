51d5c59de887aa8515781f6033693faaac9a9c3d
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-09-14 14:47:21 +0800

    feat: updated data displayed in weatherData component

--- src/components/weatherData.jsx
+++ src/components/weatherData.jsx
@@ -19,10 +19,12 @@ function WeatherData() {
     return (
         <div className="weatherDataDiv">
         {<h1>{data?.location?.country}, {data?.location?.name}</h1>}
+        {<h2>{data?.location?.region}</h2>}
         <img src={data?.current?.condition?.icon} alt="weather condition icon"></img>
-        {<p>{data?.location?.localtime}</p>}
-        {<p>In {data?.location?.name}, it is currently {data?.current?.temp_c} degrees Celcius!</p>}
-        {<p>It feels like {data?.current?.feelslike_c} degrees Celcius.</p>}
+        <p>It is currently {data?.current?.condition?.text}, with a UV index of {data?.current?.uv}</p>
+        {<p>It is currently {data?.location?.localtime}</p>}
+        {<p>It is currently {data?.current?.temp_c} degrees Celcius.</p>}
+        {<p></p>}
         </div>
     )
 }
