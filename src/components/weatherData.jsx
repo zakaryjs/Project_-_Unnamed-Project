49473ccfd1b0701963a2e024d5f893e19746a28b
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-09-14 13:26:12 +0800

    feat: updated data displayed in weatherData component and added basic styling

--- src/components/weatherData.jsx
+++ src/components/weatherData.jsx
@@ -1,3 +1,4 @@
+import '../styles/weatherData.css'
 import { useEffect, useState } from "react";
 
 
@@ -9,18 +10,19 @@ function WeatherData() {
     }, [])
 
     async function getData() {
-        let url = "http://api.weatherapi.com/v1/current.json?key=&q=melbourne&aqi=no"
+        let url = "http://api.weatherapi.com/v1/current.json?key=&q=perth&aqi=no"
         let response = await fetch(url)
         const data = await response.json()
         setData(data)
         console.log(data)
     }
     return (
-        <div>
-        <p>Testing Fetch</p>
+        <div className="weatherDataDiv">
         {<h1>{data?.location?.country}, {data?.location?.name}</h1>}
+        <img src={data?.current?.condition?.icon} alt="weather condition icon"></img>
         {<p>{data?.location?.localtime}</p>}
-        {<p>{data?.current?.temp_c} degrees Celcius!</p>}
+        {<p>In {data?.location?.name}, it is currently {data?.current?.temp_c} degrees Celcius!</p>}
+        {<p>It feels like {data?.current?.feelslike_c} degrees Celcius.</p>}
         </div>
     )
 }
