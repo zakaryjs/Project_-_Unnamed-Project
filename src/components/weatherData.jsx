307c777f5e15433fb039a7ab2d3091882f90452a
Zakary <119669322+zakaryjs@users.noreply.github.com>
2023-09-19 15:38:23 +0800

    feat: began implementing animated backgrounds for the current condition which change depending on said condition

--- src/components/weatherData.jsx
+++ src/components/weatherData.jsx
@@ -3,10 +3,14 @@ import '../styles/weatherData.css'
 function WeatherData({ weatherLocation }) {
 
     return (
-        <div className="weatherDataDiv">
-        {<h1>{weatherLocation?.location?.country}, {weatherLocation?.location?.name}</h1>}
-        {<h2>{weatherLocation?.location?.region}</h2>}
-        <h3>Right Now:</h3>
+        <div className={weatherLocation.current.condition.text === 'Patchy rain possible' ? 'patchyRain':
+        weatherLocation.current.condition.text === 'Clear' ? 'clear':
+        weatherLocation.current.condition.text === 'Sunny' ? 'clear':
+        weatherLocation.current.condition.text === 'Partly cloudy' ? 'partlyCloudy':
+        'weatherDataDiv'}>
+        {<h1 className='dataHeader'>{weatherLocation?.location?.country}, {weatherLocation?.location?.name}</h1>}
+        {<h2 className='dataHeader'>{weatherLocation?.location?.region}</h2>}
+        <h3 className='dataHeader'>Right Now:</h3>
         <img id='weatherLocationDivImage' src={weatherLocation?.current?.condition?.icon} alt="weatherLocation condition icon"></img>
         <p>Current condition: {weatherLocation?.current?.condition?.text}</p>
         <p>UV index of: {weatherLocation?.current?.uv}</p>
